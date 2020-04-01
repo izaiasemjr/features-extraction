e8061b8ba1f36afd11458614dc5641314755c1ad
izaiasjr <junymn.ufc@gmail.com>
2020-04-01 15:31:12 -0300

    build files

---
+++ build/CMakeFiles/pipeline-features-keypoints.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pipeline-features-keypoints.dir/main.cpp.o"
+  "pipeline-features-keypoints.pdb"
+  "pipeline-features-keypoints"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pipeline-features-keypoints.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
